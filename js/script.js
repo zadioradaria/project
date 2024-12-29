6a2dcaf6ff8d91ee892c8678ed037e84aa11818f
zadioradaria <zadiora.daria@leader171.kiev.ua>
2024-12-29 15:58:05 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
- document.getElementById("operation_result").onclick = function () {
+document.getElementById("operation_result").onclick = function () {
     let a = document.getElementById("First1").value;
     let b = document.getElementById("second2").value;
     let c;
-    if (document.getElementById("plus").checked) {
-        c = Number(a) + Number(b);
-    } else if (document.getElementById("minus").checked) {
-        c = a - b;
-    } else if (document.getElementById("div").checked) {
-        c = a / b;
-    } else if (document.getElementById("mult").checked) {
-        c = a * b;
-    } else {
-        с = "Операцію не було обрано";
-    }
-    document.getElementById("result").textContent = `Відповідь: ${c}`
+          if(document.getElementById("plus").checked) {
+                c = Number(a) + Number(b);
+            } else if (document.getElementById("minus").checked) {
+                c = a - b;
+            } else if (document.getElementById("div").checked) {
+                c = a / b;
+            } else if (document.getElementById("mult").checked) {
+                c = a * b;
+            } else {
+                СЃ = "РћРїРµСЂР°С†С–СЋ РЅРµ Р±СѓР»Рѕ РѕР±СЂР°РЅРѕ";
+            }
+          document.getElementById("result").textContent = `Р’С–РґРїРѕРІС–РґСЊ: ${c}`
 }
